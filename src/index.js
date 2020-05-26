e69c0e6c78fdc4e8b0e60641865688f4506ef01f
mary joseph <mary_jay@icloud.com>
2020-05-26 22:26:41 +0100

    adding error message

--- src/index.js
+++ src/index.js
@@ -2,14 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = { Lat: null, errorMessage:'' };
+
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      }
+        err => {
+            this.setState({errorMessage: err.message});
+        }
     );
+  }
+  render() {
     return (
       <div>
-        <h1>Latitude:</h1>
+        <h1>Latitude:{this.state.lat}<br />
+        Error: {this.state.errorMessage}</h1>
       </div>
     );
   }
